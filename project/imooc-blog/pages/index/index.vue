92720e515fb71ef58fe60b3953d2594ad700c727
ppambler <allinlbj@gmail.com>
2021-12-15 20:36:55 +0800

    init uni-app uni-ui project

---
+++ project/imooc-blog/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="container">
+		<view>我是你大哥</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
