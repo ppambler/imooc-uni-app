a02bc9f60ba51f8b7fc80133a4e812832a63aee8
ppambler <allinlbj@gmail.com>
2021-12-16 19:03:27 +0800

    logo 图片展示

--- project/imooc-blog/pages/hot/hot.vue
+++ project/imooc-blog/pages/hot/hot.vue
@@ -1,19 +1,25 @@
 <template>
-	<view>
-		
-	</view>
+  <view class="hot-container">
+    <!-- 微信小程序的 image 组件 -->
+    <!-- @ 符号相当于 当前项目的根目录 -->
+    <image class="logo" src="@/static/images/logo.png" mode="aspectFit" />
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.hot-container {
+  background-color: $uni-bg-color;
+  .logo {
+    width: 100%;
+    height: 80px;
+  }
+}
 </style>
