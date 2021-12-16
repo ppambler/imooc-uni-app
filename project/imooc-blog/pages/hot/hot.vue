651bcb6b77730ab04d5fb18290b5d9b48dc68b1a
ppambler <allinlbj@gmail.com>
2021-12-16 21:04:12 +0800

    创建搜索框组件

--- project/imooc-blog/pages/hot/hot.vue
+++ project/imooc-blog/pages/hot/hot.vue
@@ -3,6 +3,10 @@
     <!-- 微信小程序的 image 组件 -->
     <!-- @ 符号相当于 当前项目的根目录 -->
     <image class="logo" src="@/static/images/logo.png" mode="aspectFit" />
+    <!-- 不用注册，可直接使用组件 -->
+    <view class="search-box">
+      <my-search placeholderText="uni-app 自定义组件"></my-search>
+    </view>
   </view>
 </template>
 
@@ -20,6 +24,10 @@ export default {
   .logo {
     width: 100%;
     height: 80px;
+  }
+  .search-box {
+    padding: 0 16px;
+    margin-bottom: $uni-spacing-col-base;
   }
 }
 </style>
