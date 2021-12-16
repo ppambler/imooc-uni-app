b868378abfb430aec13c8e9e50c36766f5c2b857
ppambler <allinlbj@gmail.com>
2021-12-17 01:59:23 +0800

    update

--- project/imooc-blog/components/my-search/my-search.vue
+++ project/imooc-blog/components/my-search/my-search.vue
@@ -1,58 +1,55 @@
 <template>
-	<view class="my-search-container">
-		<!-- 搜索按钮 -->
-		<view class="my-search-box">
-			<!-- 搜索图标 -->
-			<image class="icon" src="@/static/images/search.png" mode="aspectFit" />
-			<!-- placeholder -->
-			<text class="placeholder">{{ placeholderText }}</text>
-		</view>
-
-	</view>
+  <view class="my-search-container">
+    <!-- 搜索按钮 -->
+    <view class="my-search-box">
+      <!-- 搜索图标 -->
+      <image class="icon" src="@/static/images/search.png" mode="aspectFit" />
+      <!-- placeholder -->
+      <text class="placeholder">{{ placeholderText }}</text>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		name: "my-search",
-		props: {
-			placeholderText: {
-				type: String,
-				default: '搜索'
-			}
-		},
-		data() {
-			return {
-
-			};
-		}
-	}
+export default {
+  name: "my-search",
+  props: {
+    placeholderText: {
+      type: String,
+      default: "搜索",
+    },
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-	.my-search-container {
-		display: flex;
-		align-items: center;
+.my-search-container {
+  display: flex;
+  align-items: center;
 
-		.my-search-box {
-			height: 36px;
-			width: 100%;
-			background-color: $uni-bg-color;
-			border-radius: 15px;
-			border: 1px solid $uni-bg-color-border;
-			display: flex;
-			align-items: center;
-			padding: 0 $uni-spacing-row-base;
+  .my-search-box {
+    height: 36px;
+    width: 100%;
+    background-color: $uni-bg-color;
+    border-radius: 15px;
+    border: 1px solid $uni-bg-color-border;
+    display: flex;
+    align-items: center;
+    padding: 0 $uni-spacing-row-base;
 
-			.icon {
-				width: $uni-img-size-sm;
-				height: $uni-img-size-sm;
-			}
+    .icon {
+      width: $uni-img-size-sm;
+      height: $uni-img-size-sm;
+    }
 
-			.placeholder {
-				font-size: $uni-font-size-sm;
-				margin-left: $uni-spacing-row-sm;
-				color: $uni-text-color-placeholder;
-			}
-		}
-	}
+    .placeholder {
+      font-size: $uni-font-size-sm;
+      margin-left: $uni-spacing-row-sm;
+      color: $uni-text-color-placeholder;
+    }
+  }
+}
 </style>
