78017c6d0d9735b9f3657e2edc5e9445706b3306
ppambler <allinlbj@gmail.com>
2021-12-17 11:23:52 +0800

    创建并分析  my-tabs 组件

---
+++ project/imooc-blog/components/my-tabs/my-tabs.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "my-tabs",
+		props: {
+			// 1. 可以在父组件中定制演示
+			// 配置对象
+			config: {
+				type: Object,
+				// default 如果是复杂数据类型
+				// 那么需要指定 value 为一个函数，通过 返回值 的形式指定默认值
+				default: () => {
+					return {}
+				}
+			},
+			// 2. 可以在父组件中指定数据
+			tabData: {
+				type: Array,
+				default: () => {
+					return []
+				}
+			},
+			// 3. 可以在父组件中选中项
+			defaultIndex: {
+				type: Number,
+				default: 0
+			}
+		},
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
