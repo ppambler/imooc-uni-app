94618da90dcd2e7f258e731aa40603109bb94e04
ppambler <allinlbj@gmail.com>
2021-12-17 23:09:03 +0800

    tabs 组件 - 实现滑块的滚动

--- project/imooc-blog/components/my-tabs/my-tabs.vue
+++ project/imooc-blog/components/my-tabs/my-tabs.vue
@@ -1,6 +1,6 @@
 <!--
  * @Date: 2021-12-17 11:15:21
- * @LastEditTime: 2021-12-17 18:58:39
+ * @LastEditTime: 2021-12-17 22:38:18
  * @FilePath: \imooc-blog\components\my-tabs\my-tabs.vue
 -->
 <template>
@@ -17,6 +17,7 @@
           <view class="tab-item-box">
             <block v-for="(item, index) in tabData" :key="index">
               <view
+                :id="'_tab_' + index"
                 class="tab-item"
                 :class="{ 'tab-item-active': activeIndex === index }"
                 @click="onTabClick(index)"
@@ -28,6 +29,9 @@
             class="underLine"
             :style="{
               transform: 'translateX(' + slider.left + 'px)',
+              width: defaultConfig.underLineWidth + 'px',
+              height: defaultConfig.underLineHeight + 'px',
+              backgroundColor: defaultConfig.underLineColor,
             }"
           ></view>
         </view>
@@ -72,6 +76,17 @@ export default {
         // 滑块距离左侧的距离
         left: 0,
       },
+      // 默认配置
+      defaultConfig: {
+        // 下划线宽度 px
+        underLineWidth: 24,
+        // 下划线高度 px
+        underLineHeight: 2,
+        // 下划线颜色
+        underLineColor: "#f94d2a",
+      },
+      // 内部维护的数据对象，为每个 item 单独额外维护一个 slider 的滑块对象
+      tabList: [],
     };
   },
   watch: {
@@ -79,8 +94,26 @@ export default {
     defaultIndex: {
       // 当 defaultIndex 发生变化时，回调的方法
       handler(val) {
-        console.log("立即执行");
+        console.log("defaultIndex 的 handler 立即执行");
         this.activeIndex = val;
+      },
+      // immediate：当前 handler 回调将会在侦听开始之后立即被调用
+      immediate: true,
+    },
+    // 监听 tabData 的变化
+    tabData: {
+      // 当 tabData 发生变化时，回调的方法
+      handler(val) {
+        console.log("tabData 的 handler 执行几次", val);
+        this.tabList = val;
+        // tabList 数据就已经和 tabData 同步了
+        // 维护 tabList 中的每个 item -> 在 DOM 渲染完成之后
+        // this.$nextTick() -> uni-app 虽然支持，但是 $nextTick() 存在兼容性问题
+        // 所以用了 setTimeout
+        setTimeout(() => {
+          // 计算 item 的 slider
+          this.updateTabWidth();
+        }, 0);
       },
       // immediate：当前 handler 回调将会在侦听开始之后立即被调用
       immediate: true,
@@ -90,17 +123,75 @@ export default {
     console.log(this.defaultIndex);
   },
   mounted() {
-    this.$nextTick(function () {
-      console.log(this.tabData);
-      console.log(this.defaultIndex);
-    });
+    // this.$nextTick(function () {
+    //   console.log(this.tabData);
+    //   console.log(this.defaultIndex);
+    // });
   },
   methods: {
     // tab-item 的点击事件
     onTabClick(index) {
+      console.log("click tab item");
       this.activeIndex = index;
+      this.tabToIndex();
       // 发送一个通知，通知表示激活项发生变化了
       this.$emit("tabClick", index);
+    },
+    /**
+     * 更新 tabItem 的宽度
+     */
+    updateTabWidth() {
+      console.log("updateTabWidth");
+      /**
+       * 为 tabList 的每一个 item 单独额外维护一个 slider 的滑块对象
+       */
+      let data = this.tabList;
+      if (data.length == 0) return false;
+
+      // uni-app 中 拿到渲染之后的 dom
+      // 获取 dom 的固定写法
+      // 创建一个去 SelectorQuery 的实例 -> 从当前这个 Vue 组件里边去进行获取
+      const query = uni.createSelectorQuery().in(this);
+      // 循环数据源
+      data.forEach((item, index) => {
+        // 获取 dom 的固定写法
+        query
+          .select("#_tab_" + index)
+          .boundingClientRect((res) => {
+            // res 就是拿到的 DOM 元素 -> 每个 tab item
+            // console.log(res);
+            // 为数据对象中每一个 item 都维护一个 _slider（滑动条） 对象
+            item._slider = {
+              // 当前的 tab 距离左侧的距离
+              left:
+                res.left + (res.width - this.defaultConfig.underLineWidth) / 2,
+            };
+            // 运行完成之后，计算一次「滑块」的位置
+            if (data.length - 1 === index) {
+              this.tabToIndex();
+            }
+          })
+          .exec();
+      });
+    },
+    /**
+     * 根据当前的 activeIndex，计算 滑块的滚动位置
+     */
+    tabToIndex() {
+      console.log("tabList 为空");
+      if (this.tabList.length === 0) return;
+      console.log("tabToIndex 不为空");
+      // 获取到当前的 activeIndex
+      const index = this.activeIndex;
+      // 滑块的宽度
+      const underLineWidth = this.defaultConfig.underLineWidth;
+      // TODO: 计算滑块的滚动 === this.slider.left
+      // 配置 滚动条的 left
+      this.slider = {
+        // TODO: left 怎么定义呢？
+        // left = tabItem.left + (tabItem.width - slider.width) / 2
+        left: this.tabList[this.activeIndex]._slider.left,
+      };
     },
   },
 };
