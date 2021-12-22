6db312e9735834bdb0a67cdd5778df0bfc2c3c13
ppambler <allinlbj@gmail.com>
2021-12-22 17:13:44 +0800

    热搜列表 - 数据获取

--- project/imooc-blog/api/search.js
+++ project/imooc-blog/api/search.js
@@ -1,6 +1,6 @@
 /*
  * @Date: 2021-12-21 21:21:21
- * @LastEditTime: 2021-12-21 21:21:23
+ * @LastEditTime: 2021-12-22 17:01:08
  * @FilePath: \imooc-blog\api\search.js
  */
 import request from "@/utils/request";
@@ -11,5 +11,14 @@ import request from "@/utils/request";
 export function getDefaultText() {
   return request({
     url: "/search/default-text",
+  });
+}
+
+/**
+ * 热搜搜索列表
+ */
+export function getSearchHotList() {
+  return request({
+    url: "/search/hot-list",
   });
 }
--- project/imooc-blog/components/search-hot-list/search-hot-list.vue
+++ project/imooc-blog/components/search-hot-list/search-hot-list.vue
@@ -1,20 +1,38 @@
+<!--
+ * @Date: 2021-12-21 21:51:50
+ * @LastEditTime: 2021-12-22 17:03:24
+ * @FilePath: \imooc-blog\components\search-hot-list\search-hot-list.vue
+-->
 <template>
-	<view>
-		<text>搜索列表</text>
-	</view>
+  <view>
+    <text>搜索列表</text>
+  </view>
 </template>
 
 <script>
-	export default {
-		name:"search-hot-list",
-		data() {
-			return {
-				
-			};
-		}
-	}
+import { getSearchHotList } from "@/api/search";
+
+export default {
+  name: "search-hot-list",
+  data() {
+    return {
+      hotList: [],
+    };
+  },
+  created() {
+    this.getSearchHotList();
+  },
+  methods: {
+    /**
+     * 获取热搜列表数据
+     */
+    async getSearchHotList() {
+      const { data: res } = await getSearchHotList();
+      this.hotList = res.list;
+      console.log(this.hotList);
+    },
+  },
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
