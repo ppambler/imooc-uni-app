1c71b099deb5ab128886e639806a3920a506c582
ppambler <allinlbj@gmail.com>
2021-12-25 21:45:52 +0800

    文章内容 - 解决字体过小的问题

--- project/imooc-blog/subpkg/pages/blog-detail/blog-detail.vue
+++ project/imooc-blog/subpkg/pages/blog-detail/blog-detail.vue
@@ -1,43 +1,45 @@
 <!--
  * @Date: 2021-12-25 14:04:11
- * @LastEditTime: 2021-12-25 21:18:40
+ * @LastEditTime: 2021-12-25 21:31:37
  * @FilePath: \imooc-blog\subpkg\pages\blog-detail\blog-detail.vue
 -->
 <template>
-  <view class="detail-container">
-    <!-- 文章内容区域 -->
-    <block v-if="articleData">
-      <!-- 标题 -->
-      <view class="title">{{ articleData.articleTitle }}</view>
-      <!-- 作者信息 -->
-      <view class="detail-info">
-        <view class="detail-left">
-          <view class="avatar-box">
-            <!-- 头像 -->
-            <image class="avatar" :src="articleData.avatar"></image>
+  <page-meta root-font-size="52px">
+    <view class="detail-container">
+      <!-- 文章内容区域 -->
+      <block v-if="articleData">
+        <!-- 标题 -->
+        <view class="title">{{ articleData.articleTitle }}</view>
+        <!-- 作者信息 -->
+        <view class="detail-info">
+          <view class="detail-left">
+            <view class="avatar-box">
+              <!-- 头像 -->
+              <image class="avatar" :src="articleData.avatar"></image>
+            </view>
+            <view class="author-box">
+              <!-- 作者 -->
+              <text class="author">{{ articleData.nickName }}</text>
+              <!-- 发布时间 -->
+              <text class="release-time">{{ articleData.date }}</text>
+            </view>
           </view>
-          <view class="author-box">
-            <!-- 作者 -->
-            <text class="author">{{ articleData.nickName }}</text>
-            <!-- 发布时间 -->
-            <text class="release-time">{{ articleData.date }}</text>
+          <view class="detail-right">
+            <!-- 关注按钮 -->
+            <button class="follow" size="mini">关注</button>
           </view>
         </view>
-        <view class="detail-right">
-          <!-- 关注按钮 -->
-          <button class="follow" size="mini">关注</button>
-        </view>
-      </view>
-      <!-- 文章内容 -->
-      <!-- 必须为 mp-html 增加 markdown_views 的类名 -->
-      <mp-html
-        class="markdown_views"
-        :content="addClassFromHTML(articleData.content)"
-        scroll-table
-      />
-      <!-- <rich-text :nodes="articleData.content"></rich-text> -->
-    </block>
-  </view>
+        <!-- 文章内容 -->
+        <!-- 必须为 mp-html 增加 markdown_views 的类名 -->
+        <mp-html
+          class="markdown_views"
+          :content="addClassFromHTML(articleData.content)"
+          scroll-table
+        />
+        <!-- <rich-text :nodes="articleData.content"></rich-text> -->
+      </block>
+    </view>
+  </page-meta>
 </template>
 
 <script>
